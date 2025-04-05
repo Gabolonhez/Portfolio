8a58317173a2e2a7a0575ab4575c8214979f7ecb
Gabriel BF <gbbolonhez@gmail.com>
2025-04-04 23:17:27 -0300

    feat: adding flags and cv download

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+// const cv = document.querySelector("#cv")
+
+// cv.addEventListener("click", () => {
+//   if(!confirm("Deseja baixar o meu currículo?")) {
+//     event.preventDefault()
+//   }
+// })
+
